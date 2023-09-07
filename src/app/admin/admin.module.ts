8fe3de350be59c7640ff4f4b9e5456deb25a1856
Home <ice_rain2009@mail.ru>
2023-09-07 17:48:57 +0300

    added auth

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,8 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "./shared/services/auth.service";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations:[AdminLayoutComponent,
@@ -17,6 +19,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     imports:[
         CommonModule,
         FormsModule,
+        SharedModule,
         ReactiveFormsModule,
         RouterModule.forChild([
             {path: '', component: AdminLayoutComponent, children:[
@@ -28,7 +31,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
             ]}
         ])
     ],
-    exports:[RouterModule]
+    exports:[RouterModule],
+    providers:[AuthService]
 })
 export class AdminModule {
 
