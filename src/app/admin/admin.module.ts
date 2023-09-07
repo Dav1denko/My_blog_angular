84a99b3f21cf410c5794027839c69b6372317e91
Home <ice_rain2009@mail.ru>
2023-09-07 17:00:18 +0300

    added formGroup

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
     declarations:[AdminLayoutComponent,
@@ -15,6 +16,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     EditPageComponent],
     imports:[
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild([
             {path: '', component: AdminLayoutComponent, children:[
                 {path:'', redirectTo:'/admin/login', pathMatch:'full'},
