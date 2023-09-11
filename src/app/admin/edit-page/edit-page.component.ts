2198eff5361ecb13b649ff100ee48e634e7b90a7
Home <ice_rain2009@mail.ru>
2023-09-11 13:37:46 +0300

    fix edit-page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces';
 import { PostService } from 'src/app/shared/posts.service';
 
@@ -10,17 +10,24 @@ import { PostService } from 'src/app/shared/posts.service';
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
 
   form: FormGroup
+  post: Post
+  submitted = false
+
+  uSub: Subscription
+
 
   constructor( private route:ActivatedRoute, private postsService: PostService){}
+
   ngOnInit() {
    this.route.params
    .pipe(switchMap((params:Params)=>{
     return this.postsService.getById(params['id'])
    })).subscribe((post:Post)=>{
+    this.post = post
     this.form = new FormGroup({
       title: new FormControl(post.title, Validators.required),
       text: new FormControl(post.text, Validators.required)
@@ -28,8 +35,24 @@ export class EditPageComponent implements OnInit {
 
    })
   }
+
+  ngOnDestroy() {
+    if (this.uSub){
+      this.uSub.unsubscribe()
+    }
+  }
   submit() {
-   
+    if (this.form.invalid){
+      return
+    }
+    this.submitted = true
+   this.uSub = this.postsService.update({
+    ...this.post,
+    text: this.form.value.text,
+    title: this.form.value.title
+   }).subscribe(()=>{
+    this.submitted = false
+   })
     }
 
 }
