0b95ddcf5c4381daa96835257f1c56d6a5c07928
Home <ice_rain2009@mail.ru>
2023-09-11 17:14:51 +0300

    fix post-page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostService } from '../shared/posts.service';
+import { Observable, switchMap } from 'rxjs';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
-export class PostPageComponent {
+export class PostPageComponent implements OnInit {
+
+  post$: Observable<Post>
+
+  constructor(private route: ActivatedRoute, private postsService: PostService){}
+  
+  ngOnInit() {
+   this.post$ = this.route.params
+   .pipe(switchMap((params: Params)=>{
+    return this.postsService.getById(params['id'])
+   }))
+  }
 
 }
